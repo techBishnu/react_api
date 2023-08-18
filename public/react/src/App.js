33154b6dcb09f3a95a0f8c14f2e4cffa56be5715
Bishnu <bishnu.trekkies333@gmail.com>
2023-08-18 17:35:38 +0545

    react

---
+++ public/react/src/App.js
@@ -0,0 +1,18 @@
+import Category from './components/Category';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <AppDiv >
+      
+      <Category />
+    </AppDiv>
+  );
+}
+
+export default App;
+
+const AppDiv =styled.div`
+    background-color:white;
+   
+`;
